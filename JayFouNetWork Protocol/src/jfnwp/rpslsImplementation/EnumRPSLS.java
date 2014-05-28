3c05d9f2b712d7576996c0fd2f452b1025dfaae5
djeffou <jeffrey.lucas23@gmail.com>
2014-05-28 15:48:05 +0200

    Avancement du Pierre Feuille

---
+++ JayFouNetWork Protocol/src/jfnwp/rpslsImplementation/EnumRPSLS.java
@@ -0,0 +1,14 @@
+package jfnwp.rpslsImplementation;
+
+/**
+ * List the possible moves for a player 
+ * @version 1.0
+ */
+
+public enum EnumRPSLS {
+	Rock,
+	Paper,
+	Scissors,
+	Lizard,
+	Spock;
+}
