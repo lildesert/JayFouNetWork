395213f90f63480a673f85c94e8f7cdbf2bf8a18
djeffou <jeffrey.lucas23@gmail.com>
2014-05-28 23:09:29 +0200

    l'utilisation des logs est maintenant opé. Les logs sont écrits en console et aussi dans le fichier logs.txt (qui ne doit pas être ajouté au git - d'où le gitignore)

--- JayFouNetWork Protocol/src/jfnwp/Server/RefereeServer.java
+++ JayFouNetWork Protocol/src/jfnwp/Server/RefereeServer.java
@@ -4,14 +4,16 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import jfnwp.Services.MessageService;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class RefereeServer extends Thread {
 
-	private static final int port = 8888;
+	private static final int port = 9890;
 	private static final int maxThread = 8;
 	private Socket s;
+	private static Logger logger = LogManager.getLogger(RefereeServer.class.getName());
 
 	RefereeServer(Socket sock)
 	{
@@ -22,10 +24,11 @@ public class RefereeServer extends Thread {
 	{
 		ExecutorService executorService = Executors.newFixedThreadPool(maxThread);
 		ServerSocket ssock = new ServerSocket(port);
-		System.out.println("Listening");
+		logger.info("Server start");
+		logger.info("test");
 		while (true) {
 			Socket sock = ssock.accept();
-			System.out.println("Connected");
+			logger.info("Connection of a new player");
 			executorService.execute(new RefereeServer(sock));
 		}
 	}
